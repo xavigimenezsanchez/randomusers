ee8c443bc18b21dbb10e0e6ea826aceb3010c040
Xavier Giménez Sánchez <xavierg@theprojectgroup.com>
2022-04-09 19:44:12 +0200

    Add baseRoute to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import { Home, Users, About, Error } from "./Pages";
 import "./app.scss";
 import { Menu } from "./components";
-const App = () => (
-  <div className="app">
-    <div className="app_menu">
-      <Menu />
+const App = () => {
+  const baseRoute = window.location.pathname;
+  return (
+    <div className="app">
+      <div className="app_menu">
+        <Menu />
+      </div>
+      <div className="app__body">
+        <Routes>
+          <Route path={baseRoute} element={<Home />} />
+          <Route path={`${baseRoute}users`} element={<Users />} />
+          <Route path={`${baseRoute}about`} element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
     </div>
-    <div className="app__body">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </div>
-  </div>
-);
-
+  );
+};
 export default App;
